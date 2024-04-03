c3984e1c221d4319a996015d279e13a2a1a90db5
TSY244 <1241407552@qq.com>
2024-04-03 17:32:41 +0800

    删除了虚拟环境&添加了requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,121 @@
+
+
+# 免责声明
+
+本工具仅供安全测试，严禁用于非法用途，后果与本团队无关
+
+# 简介
+
+本工具使用分布式的方式，对所指定的网络区域进行信息收集+漏洞爆破
+
+# 环境配置
+
+## elasticsearch
+
+建议使用docker 搭建，这里放一个比较简单的搭建方式
+
+https://blog.csdn.net/Acloasia/article/details/130683934
+
+## redis
+
+建议使用docker 搭建，这里放一个比较简单的搭建方式
+
+https://blog.csdn.net/weixin_45821811/article/details/116211724
+
+# 安装
+
+## scan_server
+
+考虑到需要对client 进行管理，所以直接使用python 运行就行了
+
+### 直接运行
+
+```
+pip -r requirements.txt
+python main
+```
+
+
+
+## scan_node
+
+https://github.com/TSY244/scan_node
+
+### docker
+
+检查config.ini文件，修改必要的配置
+
+```ini
+[COMMON]
+; 0: no conect to server, 1: conect to server
+mode=1
+server_ip=*
+server_port=*
+debug=0
+use_log=1
+
+[REDIS]
+redis_host=*
+redis_port=*
+redis_password=*
+message_queue=*
+
+[IP_RANGE]
+ip=192.168.79.0/24
+
+[PORT_RANGE]
+# for example: 1-65535 / 1-1000,2000-3000 / 1,2,3,4,5,6,7,8,9,10  /  1,2,3,1-100
+port=1-10000
+
+[elasticsearch]
+host=*
+port=*
+; 不填默认info
+info_index=
+; 不填默认vuls
+vuls_index=
+; user=""
+; passwd=""
+
+[web_path_scanner]
+; mode = dir or file
+; if use file_path you can scan all file's web path
+; if use file_name you can scan all web path in file
+mode=dir
+file_path=data/web_path/
+file_name=dicc.txt
+thread=30
+
+
+[subdomain_scanner]
+; mode = dir or file
+; if use file_path you can scan all file's subdomain
+; if use file_name you can scan all subdomain in file
+mode=dir 
+file_path=data/subdomain/
+file_name=dict.txt
+thread=20
+```
+
+请务必修改带`*` 部分的参数和看注释，尝试理解键名
+
+使用
+
+```
+docker build .
+```
+
+### 直接运行
+
+```python
+pip -r requirements.txt
+python main
+```
+
+
+
+
+
 # api
 
 ## info
--- cyberspaceSearchEngine/API/views.py
+++ cyberspaceSearchEngine/API/views.py
@@ -13,8 +13,9 @@ es_port = config.get("es", "port")
 info_index = config.get("es", "info_index")
 vuls_index = config.get("es", "vuls_index")
 
-info_parameters=["site","port","area","tide","vuls"]
-vuls_parameters=["site","prt_name","vul_type","vul_numb"]
+info_parameters = ["site", "port", "area", "tide", "vuls"]
+vuls_parameters = ["site", "prt_name", "vul_type", "vul_numb"]
+
 
 def search_info(request):
     global es_host, es_port, info_index, vuls_index
@@ -24,9 +25,9 @@ def search_info(request):
         body = {
             "query": {
                 "bool": {
-                "should": [
-                ],
-                "minimum_should_match": 1
+                    "should": [
+                    ],
+                    "minimum_should_match": 1
                 }
             }
         }
@@ -47,13 +48,14 @@ def search_info(request):
             for vul in get_request["vuls"].split(","):
                 body["query"]["bool"]["should"].append({"term": {"vuls": vul}})
 
-        ret=es.search_data_by_body(info_index, body=body)
+        ret = es.search_data_by_body(info_index, body=body)
         result = ret["hits"]["hits"]
         for i in range(len(result)):
             result[i] = result[i]["_source"]
         return JsonResponse(result, safe=False)
     else:
         return JsonResponse({"error": "method not allowed"}, status=405)
+
 
 def search_vuls(request):
     if request.method == "GET":
@@ -62,9 +64,9 @@ def search_vuls(request):
         body = {
             "query": {
                 "bool": {
-                "should": [
-                ],
-                "minimum_should_match": 1
+                    "should": [
+                    ],
+                    "minimum_should_match": 1
                 }
             }
         }
@@ -82,7 +84,7 @@ def search_vuls(request):
             vul_numb = get_request["vul_numb"][1:-1]
             body["query"]["bool"]["should"].append({"term": {"vul_numb.keyword": vul_numb}})
 
-        ret=es.search_data_by_body(vuls_index, body=body)
+        ret = es.search_data_by_body(vuls_index, body=body)
         result = ret["hits"]["hits"]
         for i in range(len(result)):
             result[i] = result[i]["_source"]
